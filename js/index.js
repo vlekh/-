00294d45a59893098ce7399d553cebbe96d479f5
vlekh <vlekhnovska@gmail.com>
2019-11-20 18:58:56 +0000

    Comment

--- js/index.js
+++ js/index.js
@@ -24,9 +24,17 @@ function informationAboutEducation() {
     fetch('client-data.json')
         .then( response => response.json() )
         .then( clientData =>  {
+            document.querySelector('.name').innerText = clientData.name;
+            document.querySelector('.profile').innerText = clientData.profile;
+            document.querySelector('.education').innerText = clientData.education2;
+        } );
+}
+function informationAboutEducation() {
+    fetch('client-data.json')
+        .then( response => response.json() )
+        .then( clientData =>  {
             document.querySelector('.name').innerText = clientData.name2;
             document.querySelector('.profile').innerText = clientData.profile2;
             document.querySelector('.education').innerText = clientData.education2;
         } );
 }
-
